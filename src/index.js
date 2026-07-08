90b5227f7753b6d741f63cfdc09380286a8c9836
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-07-08 15:44:33 +0100

    Add finer detail to location
    location can go down to village level

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './ui-styles.css';
 import './info-styles.css';
 import { searchCity } from './modules/api.js';
-export { displayCityErrorModal, displayLocationErrorModal };
+import { globalWData } from './modules/info-card.js';
+export { displayCityErrorModal, displayLocationErrorModal, unitToggle };
 
 // console.log(searchCity("London"));
 
@@ -39,4 +40,43 @@ function displayLocationErrorModal(city) {
   modal.showModal();
 }
 
+const unitToggle = document.getElementById('unit-toggle');
 
+unitToggle.addEventListener('change', function () {
+  if (this.checked) {
+    changeUnitToCelsius();
+  } else {
+    changeUnitToFahrenheit();
+  }
+});
+
+function changeUnitToCelsius() {
+  const unitSections = document.getElementsByClassName('temp-icon');
+  const tempTxt = document.getElementById('temp');
+  const temp = tempTxt.textContent;
+  const tempFeelTxt = document.getElementById('temp-feel');
+  const tempFeel = tempFeelTxt.textContent;
+
+  for (const section of unitSections) {
+    section.textContent = '°C';
+  }
+
+  tempTxt.textContent = Math.round(((temp - 32) * 5) / 9);
+  tempFeelTxt.textContent = Math.round(((tempFeel - 32) * 5) / 9);
+  // tempFeelTxt.textContent = (Math.round((tempFeel * 9/5) + 32));
+}
+
+function changeUnitToFahrenheit() {
+  const unitSections = document.getElementsByClassName('temp-icon');
+  const tempTxt = document.getElementById('temp');
+  const temp = tempTxt.textContent;
+  const tempFeelTxt = document.getElementById('temp-feel');
+  const tempFeel = tempFeelTxt.textContent;
+
+  for (const section of unitSections) {
+    section.textContent = '°F';
+  }
+
+  tempTxt.textContent = Math.round(globalWData.currentConditions.temp);
+  tempFeelTxt.textContent = Math.round((tempFeel * 9/5) + 32);
+}
--- src/modules/info-card.js
+++ src/modules/info-card.js
@@ -1,34 +1,70 @@
-export { renderCardInfo };
+export { renderCardInfo, globalWData};
+import { unitToggle } from '../index.js';
+
+let globalWData = null
+let globalLData = null
 
 function renderCardInfo(weatherData, locationData) {
+  const wData = weatherData;
+  const lData = locationData;
+  globalWData = weatherData;
+
+
   const conditionTxt = document.getElementById('condition');
   const cityTxt = document.getElementById('city');
-  const countryTxt = document.getElementById("country");
+  const countryTxt = document.getElementById('country');
   const tempTxt = document.getElementById('temp');
   const feelsLikeTxt = document.getElementById('temp-feel');
   const windSpeedTxt = document.getElementById('wind-speed');
   const humidityTxt = document.getElementById('humidity');
 
+  let location = lData.address.county;
+  const condition = wData.currentConditions.conditions;
+  const country = lData.address.country;
+  const currentTemp = wData.currentConditions.temp;
+  const feelsLike = wData.currentConditions.feelslike;
+  const windSpeed = wData.currentConditions.windspeed;
+  const humidity = wData.currentConditions.humidity;
 
-  const condition = weatherData.currentConditions.conditions;
-  const location = locationData.address.city;
-  const country = locationData.address.country;
-  const currentTemp = weatherData.currentConditions.temp;
-  const feelsLike = weatherData.currentConditions.feelslike;
-  const windSpeed = weatherData.currentConditions.windspeed;
-  const humidity = weatherData.currentConditions.humidity;
+  if (lData.address.city != undefined) {
+    location = lData.address.city;
+  }
+
+  if (lData.address.suburb != undefined) {
+    location = lData.address.suburb;
+  }
+
+  if (lData.address.town != undefined) {
+    location = lData.address.town;
+  }
+
+  if (lData.address.village != undefined) {
+    location = lData.address.village;
+  }
+
+  //convert to celsius based on toggle
+  // if (unitToggle.checked) {
+  //   currentTemp = convertToCelsius(currentTemp);
+  //   feelsLike = convertToCelsius(feelsLike);
+  // }
 
   conditionTxt.textContent = condition;
-  cityTxt.textContent = capitalizeFirstLetter(location);
+  cityTxt.textContent = location;
   countryTxt.textContent = country;
-  tempTxt.textContent = Math.round(currentTemp);
-  feelsLikeTxt.textContent = Math.round(feelsLike);
   windSpeedTxt.textContent = Math.round(windSpeed);
   humidityTxt.textContent = Math.round(humidity);
+  tempTxt.textContent = Math.round(currentTemp);
+  feelsLikeTxt.textContent = Math.round(feelsLike);
+
+  if (unitToggle.checked) {
+    tempTxt.textContent = Math.round(convertToCelsius(currentTemp));
+    feelsLikeTxt.textContent = Math.round(convertToCelsius(feelsLike));
+  }
+
+  console.log(location);
 }
 
-function capitalizeFirstLetter(val) {
-  const str = String(val).toLowerCase();
-  return String(str).charAt(0).toUpperCase() + String(str).slice(1);
+function convertToCelsius(input) {
+  const result = ((input - 32) * 5 / 9);
+  return result;
 }
-
